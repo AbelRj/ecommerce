2828c62dc81987c72dcdf0526c7a559866702537
Abel Ellen <abel.250.96@gmail.com>
2024-11-26 14:41:55 -0300

    Se agrega router, links al menu y la configuracion inicial de tailwindCSS

---
+++ src/components/molecules/header/Logo.jsx
@@ -0,0 +1,7 @@
+const Logo = () => {
+    return (  
+        <img src="https://edteam-media.s3.amazonaws.com/web/modules/brand/logo.png" alt="" />
+    );
+}
+ 
+export default Logo;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { API_URL } from "../../constans/env"
+
+function Home() {
+
+
+  return (
+    <>
+        <div>
+      <h1>Hola mundo {API_URL}</h1>
+      <p>Sitio de Desarrollo</p>
+    </div>
+    </>
+
+  )
+}
+
+export default Home
